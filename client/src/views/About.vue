42d47372b2e3d04fdfc267da7179e5718c40ab03
Yannik Seitz <yannikseitz@outlook.de>
2020-08-26 07:34:29 +0200

    1

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -2,8 +2,11 @@
   <div class="about">
     <h1 class="display3">About Us</h1>
     <div class="card p-3">
-      <h4>Vue Vuex Express MongoDB Tutorial</h4>
-      <p>Just lean to code LUL</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
       <span>Email:</span>mail@mail.com
     </div>
   </div>
